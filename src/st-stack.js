ee402472dda89bba10e0e69a6bcdd1657a2abc80
Andrey Voronin <AVor0n@yandex.ru>
2021-10-18 22:00:15 +0500

    feat: st-tasks complete

--- src/st-stack.js
+++ src/st-stack.js
@@ -12,19 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
- module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+module.exports = class Stack {
+  constructor() {
+    this.array = [];
+  }
+  push(value) {
+    return this.array.push(value);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array[this.array.length - 1];
   }
-}
+};
